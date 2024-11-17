e2d29177a323a4aa10726c5f57945fcaadd4b5f1
GiapDo <doquangphuong.nd2003@gmail.com>
2024-11-17 16:48:28 +0700

    lam xong dem phieu giam gia

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Coupon;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 use Surfsidemedia\Shoppingcart\Facades\Cart;
 class CartController extends Controller
 {
@@ -39,5 +42,64 @@ class CartController extends Controller
     public function empty_cart(){
         Cart::instance('cart')->destroy();
         return redirect()->back();
+    }
+
+    public function apply_coupon_code(Request $request){
+        $coupon_code = $request->coupon_code;
+        if(isset($coupon_code)){
+            $subtotal = Cart::instance('cart')->subtotal(); // Lấy tổng phụ giỏ hàng, ví dụ "14,990,000.00"
+            $subtotal = str_replace(',', '', $subtotal);  // Loại bỏ dấu phẩy
+            $subtotal = (float)$subtotal;  // Ép kiểu thành float
+
+            $coupon = Coupon::where('code', $coupon_code)
+            ->where('expiry_date', '>=', Carbon::today())
+            ->where('cart_value', '<=', $subtotal)->first();
+
+            if(!$coupon){
+                return redirect()->back()->with("error", "Invalid coupon code1!");
+            }else{
+                Session::put('coupon', [
+                    'code' => $coupon->code,
+                    'type' => $coupon->type,
+                    'value' => $coupon->value,
+                    'cart_value' => $coupon->cart_value
+                ]);
+                $this->calculateDiscount();
+                return redirect()->back()->with('success', 'Coupon has been applied!');
+            }
+        }else{
+            return redirect()->back()->with("error", "Invalid coupon code2!");
+        }
+    }
+
+    public function calculateDiscount(){
+        $discount = 0;
+        $subtotal = Cart::instance('cart')->subtotal(); // Lấy tổng phụ giỏ hàng, ví dụ "14,990,000.00"
+        $subtotal = str_replace(',', '', $subtotal);  // Loại bỏ dấu phẩy
+        $subtotal = (float)$subtotal;  // Ép kiểu thành float
+        if(Session::has('coupon')){
+            if(Session::get('coupon')['type'] == 'fixed'){
+                $discount = Session::get('coupon')['value'];
+            }else{
+                $discount = ( $subtotal * Session::get('coupon')['value'])/100;
+            }
+
+            $subtotalAfterDiscount =  $subtotal - $discount;
+            $taxAfterDiscount = ($subtotalAfterDiscount * config('cart.tax'))/100;
+            $totalAfterDiscount = $subtotalAfterDiscount + $taxAfterDiscount;
+
+            Session::put("discounts", [
+                'discount' => number_format(floatval($discount), 0, '', ','),  // Định dạng discount không có phần thập phân
+                'subtotal' => number_format(floatval($subtotalAfterDiscount), 0, '', ','),  // Định dạng subtotal
+                'tax' => number_format(floatval($taxAfterDiscount), 0, '', ','),  // Định dạng thuế
+                'total' => number_format(floatval($totalAfterDiscount), 0, '', ',')  // Định dạng tổng
+            ]);
+        }
+    }
+    
+    public function remove_coupon_code(){
+        Session::forget('coupon');
+        Session::forget('discounts');
+        return back()->with('success', 'Coupon has been removed!');
     }
 }
--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 @section('content')
+<style>
+  .text-success{
+    color: #278c04 !important;
+  }
+  .text-danger{
+    color: #d61808 !important;
+  }
+</style>
 <main class="pt-90">
     <div class="mb-4 pb-4"></div>
     <section class="shop-checkout container">
@@ -96,27 +104,52 @@
             </tbody>
           </table>
           <div class="cart-table-footer">
-            <form action="#" class="position-relative bg-body">
-              <input class="form-control" type="text" name="coupon_code" placeholder="Coupon Code">
-              <input class="btn-link fw-medium position-absolute top-0 end-0 h-100 px-4" type="submit"
-                value="APPLY COUPON">
+            @if (!Session::has('coupon'))
+            <form action="{{route('cart.coupon.apply')}}" method="POST" class="position-relative bg-body">
+              @csrf
+              <input class="form-control" type="text" name="coupon_code" placeholder="Coupon Code" value="">
+              <input class="btn-link fw-medium position-absolute top-0 end-0 h-100 px-4" type="submit" value="APPLY COUPON">
             </form>
+            @else
+            <form action="{{route('cart.coupon.remove')}}" method="POST" class="position-relative bg-body">
+              @csrf
+              @method('DELETE')
+              <input class="form-control" type="text" name="coupon_code" placeholder="Coupon Code" value="{{ Session::has('coupon') ? Session::get('coupon')['code'].' Applied!': '' }}">
+              <input class="btn-link fw-medium position-absolute top-0 end-0 h-100 px-4" type="submit" value="REMOVE COUPON">
+            </form>
+            @endif
             <form method="POST" action="{{route('cart.empty')}}">
               @csrf
               @method('DELETE')
               <button class="btn btn-light" type="submit">CLEAR CART</button>
             </form>
           </div>
+          <div>
+            @if (Session::has('success'))
+                <p class="text-success"> {{Session::get('success')}}</p>
+            @elseif(Session::has('error'))
+              <p class="text-danger"> {{Session::get('error')}}</p>
+            @endif
+          </div>
         </div> 
         <div class="shopping-cart__totals-wrapper">
           <div class="sticky-content">
             <div class="shopping-cart__totals">
               <h3>Cart Totals</h3>
+              @if (Session::has('discounts'))
               <table class="cart-totals">
                 <tbody>
                   <tr>
                     <th>Subtotal</th>
-                    <td>${{Cart::instance('cart')->subTotal()}}</td>
+                    <td>{{Cart::instance('cart')->subTotal()}}VNĐ</td>
+                  </tr>
+                  <tr>
+                    <th>Discount: {{Session::get('coupon')['code']}}</th>
+                    <td>{{Session::get('discounts')['discount']}}VNĐ</td>
+                  </tr>
+                  <tr>
+                    <th>Subtotal After Discount</th>
+                    <td>{{Session::get('discounts')['subtotal']}}VNĐ</td>
                   </tr>
                   <tr>
                     <th>Shipping</th>
@@ -126,14 +159,38 @@
                   </tr>
                   <tr>
                     <th>VAT</th>
-                    <td>${{Cart::instance('cart')->tax()}}</td>
+                    <td>{{Session::get('discounts')['tax']}}VNĐ</td>
                   </tr>
                   <tr>
                     <th>Total</th>
-                    <td>${{Cart::instance('cart')->total()}}</td>
+                    <td>{{Session::get('discounts')['total']}}VNĐ</td>
                   </tr>
                 </tbody>
               </table>
+              @else
+              <table class="cart-totals">
+                <tbody>
+                  <tr>
+                    <th>Subtotal</th>
+                    <td>{{Cart::instance('cart')->subTotal()}}VNĐ</td>
+                  </tr>
+                  <tr>
+                    <th>Shipping</th>
+                    <td>
+                      Free
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>VAT</th>
+                    <td>{{Cart::instance('cart')->tax()}}VNĐ</td>
+                  </tr>
+                  <tr>
+                    <th>Total</th>
+                    <td>{{Cart::instance('cart')->total()}}VNĐ</td>
+                  </tr>
+                </tbody>
+              </table>
+              @endif
             </div>
             <div class="mobile_fixed-btn_wrapper">
               <div class="button-wrapper container">
